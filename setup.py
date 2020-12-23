29c14f1ab5e04dc9b5d8ceb25c4144946d1d61e3
Kwonil Kim <kwonil.kim.0@gmail.com>
2020-12-23 10:31:44 +0900

    Fix setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1',
     packages=find_packages(exclude=['tests*']),
     package_dir={'kkpy': './kkpy'},
-    package_data={'kkpy': 'SHP/*'},
+    package_data={'kkpy': ['SHP/*']},
     license='MIT',
     description='My python package',
     long_description=open('README.md').read(),
