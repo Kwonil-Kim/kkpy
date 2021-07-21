cd16d9c521959aea75cbdbc4f7a469a8398c3d00
Kwonil Kim <kwonil.kim.0@gmail.com>
2021-07-21 16:20:07 +0900

    Add cm.wind

--- kkpy/cm.py
+++ kkpy/cm.py
@@ -17,6 +17,7 @@ wind
     kkpy.cm.precip
     kkpy.cm.precip_kma
     kkpy.cm.precip_kma_aws
+    kkpy.cm.wind
 
 """
 import numpy as np
@@ -32,13 +33,13 @@ def refl(levels=None, snow=False):
     ---------
     >>> cmap = kkpy.cm.refl()
     >>> pm = ax.pcolormesh(lon2d, lat2d, ref2d.T, cmap=cmap['cmap'], norm=cmap['norm'])
-    >>> plt.colorbar(pm, cmap=cmap['cmap'], ticks=cmap['ticks'])
+    >>> plt.colorbar(pm, ticks=cmap['ticks'])
     
     Parameters
     ----------
-    levels : array_like
+    levels : array_like, optional
         Array containing user-defined color tick levels. It should have lower and upper bounds.
-    snow : boolean
+    snow : boolean, optional
         Return broader tick levels suitable for snow. Default is False.
         
     Returns
@@ -77,11 +78,11 @@ def doppler(levels=None):
     ---------
     >>> cmap = kkpy.cm.doppler()
     >>> pm = ax.pcolormesh(lon2d, lat2d, VD2d.T, cmap=cmap['cmap'], norm=cmap['norm'])
-    >>> plt.colorbar(pm, cmap=cmap['cmap'], ticks=cmap['ticks'])
+    >>> plt.colorbar(pm, ticks=cmap['ticks'])
     
     Parameters
     ----------
-    levels : array_like
+    levels : array_like, optional
         Array containing user-defined color tick levels. It should have lower and upper bounds.
         
     Returns
@@ -112,11 +113,11 @@ def zdr(levels=None):
     ---------
     >>> cmap = kkpy.cm.zdr()
     >>> pm = ax.pcolormesh(lon2d, lat2d, zdr2d.T, cmap=cmap['cmap'], norm=cmap['norm'])
-    >>> plt.colorbar(pm, cmap=cmap['cmap'], ticks=cmap['ticks'])
+    >>> plt.colorbar(pm, ticks=cmap['ticks'])
     
     Parameters
     ----------
-    levels : array_like
+    levels : array_like, optional
         Array containing user-defined color tick levels. It should have lower and upper bounds.
         
     Returns
@@ -147,11 +148,11 @@ def kdp(levels=None):
     ---------
     >>> cmap = kkpy.cm.kdp()
     >>> pm = ax.pcolormesh(lon2d, lat2d, kdp2d.T, cmap=cmap['cmap'], norm=cmap['norm'])
-    >>> plt.colorbar(pm, cmap=cmap['cmap'], ticks=cmap['ticks'])
+    >>> plt.colorbar(pm, ticks=cmap['ticks'])
     
     Parameters
     ----------
-    levels : array_like
+    levels : array_like, optional
         Array containing user-defined color tick levels. It should have lower and upper bounds.
         
     Returns
@@ -182,11 +183,11 @@ def rhohv(levels=None):
     ---------
     >>> cmap = kkpy.cm.rhohv()
     >>> pm = ax.pcolormesh(lon2d, lat2d, rhohv2d.T, cmap=cmap['cmap'], norm=cmap['norm'])
-    >>> plt.colorbar(pm, cmap=cmap['cmap'], ticks=cmap['ticks'])
+    >>> plt.colorbar(pm, ticks=cmap['ticks'])
     
     Parameters
     ----------
-    levels : array_like
+    levels : array_like, optional
         Array containing user-defined color tick levels. It should have lower and upper bounds.
         
     Returns
@@ -217,13 +218,13 @@ def precip(levels=None, coarse_ticks=False):
     ---------
     >>> cmap = kkpy.cm.precip()
     >>> pm = ax.pcolormesh(lon2d, lat2d, prec2d.T, cmap=cmap['cmap'], norm=cmap['norm'])
-    >>> plt.colorbar(pm, cmap=cmap['cmap'], ticks=cmap['ticks'])
+    >>> plt.colorbar(pm, ticks=cmap['ticks'])
     
     Parameters
     ----------
-    levels : array_like
+    levels : array_like, optional
         Array containing user-defined color tick levels. It should have lower and upper bounds.
-    coarse_ticks : boolean
+    coarse_ticks : boolean, optional
         True if colorbar levels are too dense.
         
     Returns
@@ -256,13 +257,13 @@ def precip_kma(levels=None, coarse_ticks=False):
     ---------
     >>> cmap = kkpy.cm.precip_kma()
     >>> pm = ax.pcolormesh(lon2d, lat2d, prec2d.T, cmap=cmap['cmap'], norm=cmap['norm'])
-    >>> plt.colorbar(pm, cmap=cmap['cmap'], ticks=cmap['ticks'])
+    >>> plt.colorbar(pm, ticks=cmap['ticks'])
     
     Parameters
     ----------
-    levels : array_like
+    levels : array_like, optional
         Array containing user-defined color tick levels. It should have lower and upper bounds.
-    coarse_ticks : boolean
+    coarse_ticks : boolean, optional
         True if colorbar levels are too dense.
         
     Returns
@@ -295,13 +296,13 @@ def precip_kma_aws(levels=None, coarse_ticks=False):
     ---------
     >>> cmap = kkpy.cm.precip_kma_aws()
     >>> pm = ax.pcolormesh(lon2d, lat2d, prec2d.T, cmap=cmap['cmap'], norm=cmap['norm'])
-    >>> plt.colorbar(pm, cmap=cmap['cmap'], ticks=cmap['ticks'])
+    >>> plt.colorbar(pm, ticks=cmap['ticks'])
     
     Parameters
     ----------
-    levels : array_like
+    levels : array_like, optional
         Array containing user-defined color tick levels. It should have lower and upper bounds.
-    coarse_ticks : boolean
+    coarse_ticks : boolean, optional
         True if colorbar levels are too dense.
         
     Returns
@@ -326,6 +327,43 @@ def precip_kma_aws(levels=None, coarse_ticks=False):
     
     return dict_cmap
 
+def wind(levels=None):
+    """
+    Wind direction colors (cyclic).
+    
+    Examples
+    ---------
+    >>> cmap = kkpy.cm.wind()
+    >>> pm = ax.pcolormesh(lon2d, lat2d, winddir.T, cmap=cmap['cmap'], norm=cmap['norm'])
+    >>> cb = plt.colorbar(pm, ticks=cmap['ticks'])
+    >>> cb.set_ticklabels(['N', 'E', 'S', 'W', 'N'])
+    
+    Parameters
+    ----------
+    levels : array_like, optional
+        Array containing user-defined color tick levels. It should have lower and upper bounds.
+        
+    Returns
+    ---------
+    dict_cmap : dictionary
+        'cmap': matplotlib colormap
+        'norm': color tick levels for plot
+        'ticks': color tick levels for colorbar
+    """
+    
+    if levels is None:
+        level = [0,90,180,270,360]
+    else:
+        level = levels
+    
+    cmap = _wind_color()
+    
+    dict_cmap = {}
+    dict_cmap['cmap'] = cmap
+    dict_cmap['norm'] = col.BoundaryNorm(np.linspace(0,360,256), ncolors=256)
+    dict_cmap['ticks'] = level
+    
+    return dict_cmap
 
 def _ref_color():
     colors = ['#ffffff',
@@ -402,6 +440,36 @@ def _vel_color_vpr_snow():
     plt.cm.register_cmap(name='knuvel', cmap=cmap_colors)
     return cmap_colors
 
+def _wind_color():
+    colors = ['#8000c3','#7e00c4','#7c00c5','#7a00c6','#7800c7','#7600c8','#7400c8','#7200c9','#7000ca','#6e00cb',
+              '#6c00cc','#6a00cd','#6800ce','#6600cf','#6400d0','#6200d1','#6000d2','#5e00d3','#5c00d4','#5a00d5',
+              '#5800d6','#5600d7','#5400d8','#5200d9','#5000da','#4e00db','#4c00dc','#4a00dd','#4800de','#4600de',
+              '#4400df','#4200e0','#4000e1','#3e00e2','#3c00e3','#3a00e4','#3800e5','#3600e6','#3400e7','#3200e8',
+              '#3000e9','#2e00ea','#2c00eb','#2a00ec','#2800ed','#2600ee','#2400ef','#2200f0','#2000f1','#1e00f2',
+              '#1c00f3','#1a00f4','#1800f5','#1600f5','#1400f6','#1200f7','#1000f8','#0e00f9','#0c00fa','#0a00fb',
+              '#0800fc','#0600fd','#0400fe','#0200ff','#0101ff','#0505fb','#0909f7','#0d0df3','#1111ef','#1515eb',
+              '#1919e7','#1d1de3','#2121df','#2525db','#2929d7','#2d2dd3','#3131cf','#3535cb','#3939c7','#3d3dc3',
+              '#4141bf','#4545bb','#4949b7','#4d4db3','#5151af','#5555ab','#5959a7','#5d5da2','#61619e','#65659a',
+              '#696996','#6d6d92','#71718e','#75758a','#797986','#7d7d82','#81817e','#85857a','#898976','#8d8d72',
+              '#91916e','#95956a','#999966','#9d9d62','#a1a15e','#a5a55a','#aaaa56','#aeae52','#b2b24e','#b6b64a',
+              '#baba46','#bebe42','#c2c23e','#c6c63a','#caca36','#cece32','#d2d22e','#d6d62a','#dada26','#dede22',
+              '#e2e21e','#e6e61a','#eaea16','#eeee12','#f2f20e','#f6f60a','#fafa06','#fefe02','#fffe00','#fffa00',
+              '#fff600','#fff200','#ffee00','#ffea00','#ffe600','#ffe200','#ffde00','#ffda00','#ffd600','#ffd200',
+              '#ffce00','#ffca00','#ffc600','#ffc200','#ffbe00','#ffba00','#ffb600','#ffb200','#ffae00','#ffaa00',
+              '#ffa600','#ffa100','#ff9d00','#ff9900','#ff9500','#ff9100','#ff8d00','#ff8900','#ff8500','#ff8100',
+              '#ff7d00','#ff7900','#ff7500','#ff7100','#ff6d00','#ff6900','#ff6500','#ff6100','#ff5d00','#ff5900',
+              '#ff5500','#ff5100','#ff4d00','#ff4900','#ff4500','#ff4100','#ff3d00','#ff3900','#ff3500','#ff3100',
+              '#ff2d00','#ff2900','#ff2500','#ff2100','#ff1d00','#ff1900','#ff1500','#ff1100','#ff0d00','#ff0900',
+              '#ff0500','#ff0100','#fe0002','#fc0005','#fa0008','#f8000b','#f6000e','#f40011','#f20015','#f00018',
+              '#ee001b','#ec001e','#ea0021','#e80024','#e60027','#e4002a','#e2002d','#e00030','#de0033','#dc0036',
+              '#da0039','#d8003c','#d6003f','#d40042','#d20045','#d00048','#ce004b','#cc004e','#ca0051','#c80054',
+              '#c60057','#c4005b','#c2005e','#c00061','#be0064','#bc0067','#ba006a','#b8006d','#b60070','#b40073',
+              '#b20076','#b00079','#ae007c','#ac007f','#aa0082','#a80085','#a60088','#a4008b','#a2008e','#a00091',
+              '#9e0094','#9c0097','#9a009a','#98009d','#9600a0','#9400a4','#9200a7','#9000aa','#8e00ad','#8c00b0',
+              '#8a00b3','#8800b6','#8600b9','#8400bc','#8200bf','#8000c2']
+    cmap_colors = col.ListedColormap(colors)
+    plt.cm.register_cmap(name='kkpywind', cmap=cmap_colors)
+    return cmap_colors
 
 # LEVELS
 def _z_level_rain():
