ae6aedd88d039f6a031963ff646e4c148ef92713
Kwonil Kim <kwonil.kim.0@gmail.com>
2021-11-19 10:09:00 +0900

    Add reader func: vertix, vet, hsr, sounding, lidar_wind, wpr_kma, pluvio_raw

--- kkpy/io.py
+++ kkpy/io.py
@@ -13,10 +13,18 @@ Functions to read and write files
     kkpy.io.read_mxpol_rhi_with_hc
     kkpy.io.read_dem
     kkpy.io.read_wissdom
+    kkpy.io.read_vertix
+    kkpy.io.read_vet
+    kkpy.io.read_hsr
+    kkpy.io.read_sounding
+    kkpy.io.read_lidar_wind
+    kkpy.io.read_wpr_kma
+    kkpy.io.read_pluvio_raw
 
 """
 import numpy as np
 import pandas as pd
+import xarray as xr
 import datetime
 import glob
 import os
@@ -24,7 +32,7 @@ import sys
 
 def read_aws(time, date_range=True, datadir='/disk/STORAGE/OBS/AWS/', stnid=None, dask=True):
     """
-    Read AWS_MIN files into dataframe.
+    Read AWS (AWS_MIN).
     
     Examples
     ---------
@@ -52,6 +60,10 @@ def read_aws(time, date_range=True, datadir='/disk/STORAGE/OBS/AWS/', stnid=None
     ---------
     df_aws : dataframe
         Return dataframe of aws data.
+    
+    Warnings
+    ---------
+    This routine will be updated to receive argument for only filenames. The syntax will be likely changed in the near future.
     """
     import dask.dataframe as dd
     
@@ -390,7 +402,7 @@ def read_dem(file=None, area='pyeongchang'):
 
 def get_fname(indir, pattern, dt, date_range=True, verbose=True):
     """
-    Get filename matching the datetime and format.
+    Get filename corresponds to the given datetime(s) and format.
     
     Examples
     ---------
@@ -1049,3 +1061,625 @@ def read_wissdom(fnames, kind='KNUv2', degree='essential'):
         raise UserWarning(f'Not supported: kind={kind}')
     
     return ds
+
+def read_vertix(fnames):
+    """
+    Read VertiX (nc).
+    
+    Examples
+    ---------
+    >>> ds_vtx = kkpy.io.read_vertix(fnames)
+    
+    Parameters
+    ----------
+    fnames : str or array_like
+        Filename(s) of VertiX to read.
+    
+    Returns
+    ---------
+    ds : xarray dataset object
+        Return VertiX dataset.
+    """
+    from cftime import num2date
+    import xarray as xr
+    
+    # read data
+    ds = xr.open_mfdataset(
+        fnames,
+        decode_times=False,
+        combine='nested'
+    )
+    
+    # calculate datetime
+    for time_var in ['time_dwell', 'time_spec']:
+        ds[time_var] = xr.DataArray(
+            num2date(
+                ds[time_var].data,
+                'seconds since 1970-01-01 00:00:00.0',
+                only_use_cftime_datetimes=False,
+                only_use_python_datetimes=True
+            ),
+            dims=[time_var]
+        )
+    
+    # variable to coordinate
+    ds = ds.set_coords('range')
+    ds = ds.set_coords('spec_vms')
+    
+    # replace nodata to NaN
+    for var in list(ds.keys()):
+        ds[var] = ds[var].where(ds[var] > ds.nodata)
+        
+    return ds
+
+def read_vet(fnames):
+    """
+    Read WRC VET motion vector.
+    
+    Examples
+    ---------
+    >>> ds_vet = kkpy.io.read_vet(fnames)
+    
+    Parameters
+    ----------
+    fnames : str or array_like
+        Filename(s) of VET motion vector to read.
+    
+    Returns
+    ---------
+    ds : xarray dataset object
+        Return WRC VET motion vector dataset.
+    """
+    import xarray as xr
+    
+    dss = []
+    
+    if isinstance(fnames, (list, np.ndarray)):
+        for i_f, fname in enumerate(fnames):
+            _ds = _read_vet(fname)
+            dss.append(_ds)
+        ds = xr.concat(dss, dim='t')
+    else:
+        ds = _read_vet(fnames)
+    
+    return ds
+    
+
+def _read_vet(fname):
+    import cartopy.crs as ccrs
+    
+    nx_vet = 25
+    ny_vet = 25
+    dxy_vet = 32000 # meter
+    border_vet = 112
+    clon_vet = 126.0
+    clat_vet = 38.0
+    fe_vet = 440000
+    fn_vet = 770000
+
+    proj_vet = ccrs.LambertConformal(
+        central_longitude=clon_vet,
+        central_latitude=clat_vet,
+        false_easting=fe_vet,
+        false_northing=fn_vet,
+        standard_parallels=(30,60)
+    )
+    
+    # READ DATA
+    dt = np.dtype([
+        ('t','<6i2'),
+        ('pixel_res_km','<f4'),
+        ('border_pix','<i4'),
+        ('ew_arsize','<i4'),
+        ('sn_arsize','<i4'),
+        ('velf_ew_size','<i4'),
+        ('velf_sn_size','<i4'),
+        ('uv', '(25,25,2)f4')]
+    )
+    data = np.fromfile(fname, dtype=dt)
+    
+    # Resolution of vector data, assuming that N-S and E-W grids are same
+    cnt_vec = data['velf_ew_size'][0]
+    cnt_border = data['border_pix'][0]
+    cnt_pixel = data['ew_arsize'][0]
+    res_vec = (cnt_pixel - 2*cnt_border)/cnt_vec
+    
+    # DEFINE DATASET
+    ds = xr.Dataset(
+        {
+            'u': (["t","y","x"], data['uv'][0,:,:,0][None,:,:]),
+            'v': (["t","y","x"], data['uv'][0,:,:,1][None,:,:]),
+        },
+        coords={
+            'x': (["x"],
+                (np.arange(cnt_vec)*res_vec + res_vec/2. + cnt_border)*1e3),
+            'y': (["y"],
+                (np.arange(cnt_vec)*res_vec + res_vec/2. + cnt_border)*1e3),
+            't': (["t"], [pd.Timestamp(os.path.basename(fname)[-16:-4])]),
+        },
+        attrs={
+            'title': 'Output of variational echo tracking by KMA',
+            'time_unit': 'KST',
+            'vector_resolution': f'{res_vec} km',
+            'crs': proj_vet.proj4_init,
+            'projection': proj_vet,
+        }
+    )
+    
+    return ds
+
+def read_hsr(fnames):
+    """
+    Read WRC HSR reflectivity.
+    
+    Examples
+    ---------
+    >>> ds_vet = kkpy.io.read_hsr(fnames)
+    
+    Parameters
+    ----------
+    fnames : str or array_like
+        Filename(s) of HSR reflectivity to read.
+    
+    Returns
+    ---------
+    ds : xarray dataset object
+        Return WRC HSR reflectivity dataset.
+    """
+    import xarray as xr
+    
+    dss = []
+    
+    if isinstance(fnames, (list, np.ndarray)):
+        for i_f, fname in enumerate(fnames):
+            _ds = _read_hsr(fname)
+            dss.append(_ds)
+        ds = xr.concat(dss, dim='t')
+    else:
+        ds = _read_hsr(fnames)
+    
+    return ds
+
+def _read_hsr(file):
+    import gzip
+    import cartopy.crs as ccrs
+    
+    def bin2str(binary):
+        return [ord(c) for c in binary.decode('ascii')]
+
+    def timestr2dt(file):
+        yy = np.frombuffer(file.read(2), dtype=np.int16)[0]
+        mm = ord(file.read(1))
+        dd = ord(file.read(1))
+        hh = ord(file.read(1))
+        mi = ord(file.read(1))
+        ss = ord(file.read(1))
+        try:
+            return datetime.datetime(yy,mm,dd,hh,mi,ss)
+        except:
+            return -1
+
+    with gzip.open(file,'rb') as f:
+        version    = ord(f.read(1)) # char
+        ptype      = np.frombuffer(f.read(2), dtype=np.int16)[0] # short
+        tm         = timestr2dt(f) # struct
+        tm_in      = timestr2dt(f) # struct
+        num_stn    = ord(f.read(1)) # char
+        map_code   = ord(f.read(1)) # char
+        map_etc    = ord(f.read(1)) # char
+        nx         = np.frombuffer(f.read(2), dtype=np.int16)[0] # short
+        ny         = np.frombuffer(f.read(2), dtype=np.int16)[0] # short
+        nz         = np.frombuffer(f.read(2), dtype=np.int16)[0] # short
+        dxy        = np.frombuffer(f.read(2), dtype=np.int16)[0] # short
+        dz         = np.frombuffer(f.read(2), dtype=np.int16)[0] # short
+        z_min      = np.frombuffer(f.read(2), dtype=np.int16)[0] # short
+        num_data   = ord(f.read(1)) # char
+        data_code  = bin2str(f.read(16)) # char
+        etc        = bin2str(f.read(15)) # char
+        
+        for ii in range(48):
+            stn_cd = bin2str(f.read(6)) # char
+            _tm    = timestr2dt(f) # struct
+            _tm_in = timestr2dt(f) # struct
+        
+        refl       = np.frombuffer(f.read(2*nx*ny), dtype=np.int16)
+        
+    refl = refl.copy().reshape(ny,nx) / 100
+    refl[refl == -300] = -9999 # outside radar observable range
+    refl[refl == -250] = -9998 # no echo
+    refl[(refl < -100) & (refl > -9000)] = -9997 # null
+    
+    # hsr = {}
+    # hsr['reflectivity'] = refl
+    # hsr['nx'] = nx
+    # hsr['ny'] = ny
+    # hsr['dxy'] = dxy # meter
+    # hsr['clon'] = clon_hsr # degN
+    # hsr['clat'] = clat_hsr # degE
+    # hsr['false_easting'] = fe_hsr # meter
+    # hsr['false_northing'] = fn_hsr # meter
+    # hsr['cnt_radar'] = num_stn
+    # hsr['time'] = tm # datetime object
+    
+    clon_hsr = 126.0
+    clat_hsr = 38.0
+    fe_hsr = 560500 # meter
+    fn_hsr = 840500 # meter
+    proj_hsr = ccrs.LambertConformal(
+        central_longitude=clon_hsr,
+        central_latitude=clat_hsr,
+        false_easting=fe_hsr,
+        false_northing=fn_hsr,
+        standard_parallels=(30,60)
+    )
+    
+    # DEFINE DATASET
+    ds = xr.Dataset(
+        {
+            'reflectivity': (["t","y","x"], refl[None,:,:]),
+        },
+        coords={
+            'x': (["x"],
+                np.arange(0,np.int64(nx)*dxy, dxy)),
+            'y': (["y"],
+                np.arange(0,np.int64(ny)*dxy, dxy)),
+            't': (["t"], [pd.Timestamp(os.path.basename(file)[-19:-7])]),
+        },
+        attrs={
+            'title': 'HSR product generated by KMA',
+            'time_unit': 'KST',
+            'cnt_radar': num_stn,
+            'outside_radar_observable_range': -9999,
+            'no_echo': -9998,
+            'null': -9997,
+            'crs': proj_hsr.proj4_init,
+            'projection': proj_hsr,
+        }
+    )
+    
+    return ds
+
+
+def read_sounding(fnames):
+    """
+    Read sounding.
+    
+    Examples
+    ---------
+    >>> df = kkpy.io.read_sounding(fnames)
+    
+    Parameters
+    ----------
+    fnames : str or array_like
+        Filename(s) of sounding to read.
+    
+    Returns
+    ---------
+    df : pandas dataframe object
+        Return sounding dataframe.
+    
+    Notes
+    ---------
+    Columns:
+        - 'P': pressure [hPa]
+        - 'T': temperature [degC]
+        - 'RH': relative humidity [%]
+        - 'WS': wind speed [m s-1]
+        - 'WD': wind direction [deg]
+        - 'Lon': longitude [oE]
+        - 'Lat': latitude [oN]
+        - 'Alt': altitude from mean sea level [m]
+        - 'Geo': geopotential height [gpm]
+        - 'Dew': dew-point temperature [degC]
+        - 'U': u-wind [m s-1]
+        - 'V': v-wind [m s-1]
+        - 'Uknot': u-wind [knot]
+        - 'Vknot': v-wind [knot]
+        - 'time': launch time [file-dependent, generally UTC]
+    """
+    import pandas as pd
+    
+    dfs = []
+    
+    if isinstance(fnames, (list, np.ndarray)):
+        for i_f, fname in enumerate(fnames):
+            _df = _read_sounding(fname)
+            dfs.append(_df)
+        df = pd.concat(dfs).reset_index(drop=True)
+    else:
+        df = _read_sounding(fnames)
+    
+    return df
+
+def _read_sounding(filename):
+    from . import util
+
+    def read_sounding_v1(filename):
+        df = pd.read_csv(filename,
+                         delim_whitespace=True,
+                         skiprows=10,
+                         on_bad_lines='skip',
+                         encoding='euc-kr'
+                        )
+        df = df.drop(['Time(min:sec)', 'Asc(m/m)'], axis=1)
+        df.columns = ['P', 'T', 'RH', 'WS', 'WD', 'Lon', 'Lat', 'Alt','Geo', 'Dew']
+        df['WS'] = util.knot2ms(df['WS'])
+        return df
+
+    def read_sounding_v2(filename):
+        df = pd.read_csv(filename,
+                         delim_whitespace=True,
+                         skiprows=5,
+                         on_bad_lines='skip',
+                         encoding='ISO-8859-1'
+                        )
+        df = df.drop(['min', 's', 'm/s.1'], axis=1)
+        df.columns = ['P', 'T', 'RH', 'WS', 'WD', 'Alt', 'Geo', 'Dew', 'Lat', 'Lon']
+        # df['WS'] = util.ms2knot(df['WS'])
+        return df
+
+    def clean_df(df):
+        def to_float(x):
+            try:
+                return float(x)
+            except ValueError:
+                return None
+
+        list_cols = ['P', 'T', 'RH', 'WS', 'WD', 'Alt','Geo', 'Dew', 'Lat', 'Lon']
+
+        _df = df
+        for col in list_cols:
+            valid_index = _df[col].apply(to_float).dropna().index
+            _df = _df.loc[valid_index]
+
+        for col in list_cols:
+            _df[col] = _df[col].astype('float64')
+
+        return _df
+    
+    def get_launch_time(fname):
+        try:
+            with open(fname, encoding='CP949') as f:
+                hdr = f.readline()
+                launch_time = datetime.datetime.strptime(hdr[31:50], '%Y-%m-%d %H:%M:%S')
+        except:
+            try:
+                with open(fname, encoding='ISO-8859-1') as f:
+                    mdy = f.readline()[-9:-1]
+                    his = f.readline()[-9:-1]
+                    launch_time = pd.to_datetime(f'{mdy} {his}', format='%d/%m/%y %H:%M:%S')
+            except:
+                raise UserWarning(f'Failed reading launch time : {fname}')
+        return launch_time
+    
+    try:
+        df = read_sounding_v1(filename)
+    except:
+        try:
+            df = read_sounding_v2(filename)
+        except:
+            raise UserWarning(f'Cannot read this kind of format: {filename}')
+
+    df = clean_df(df)
+    df['U'], df['V'] = util.wind2uv(wd=df['WD'], ws=df['WS'])
+    df['Uknot'], df['Vknot'] = util.ms2knot(df['U']), util.ms2knot(df['V'])
+    df['time'] = get_launch_time(filename)
+    
+    return df
+
+def _read_lidar_wind(fname):
+    from . import util
+    
+    df = pd.read_csv(
+        fname,
+        skiprows=2,
+        delim_whitespace=True,
+        names=['Alt', 'U', 'V', 'W', 'WS', 'WD', 'Valid'],
+        na_values=-999,
+    )
+    df['Uknot'], df['Vknot'] = util.ms2knot(df['U']), util.ms2knot(df['V'])
+    
+    return df
+
+def read_lidar_wind(fnames, ftimes, dropna=True):
+    """
+    Read lidar wind profile.
+    
+    Examples
+    ---------
+    >>> df = kkpy.io.read_lidar_wind(fnames)
+    
+    Parameters
+    ----------
+    fnames : str or array_like
+        Filename(s) of lidar wind profile to read.
+    ftimes : str or array_like
+        Datetime(s) of lidar wind profile to read (see: kkpy.io.get_fname).
+    dropna : boolean (optional)
+        True if drop rows containing NaN values.
+    
+    Returns
+    ---------
+    df : pandas dataframe object
+        Return lidar wind profile dataframe.
+    
+    Notes
+    ---------
+    Columns:
+        - 'Alt': altitude from lidar [m]
+        - 'U': u-wind [m s-1]
+        - 'V': v-wind [m s-1]
+        - 'W': w-wind [m s-1]
+        - 'WS': wind speed [m s-1]
+        - 'WD': wind direction [deg]
+        - 'Valid': Proportion of the number of data available for wind calculation along the azimuth direction [%]
+        - 'Uknot': u-wind [knot]
+        - 'Vknot': v-wind [knot]
+        - 'time': measurement time [file-dependent, generally KST]
+    """
+
+    dfs = []
+    
+    if isinstance(fnames, (list, np.ndarray)):
+        for i_f, fname in enumerate(fnames):
+            _df = _read_lidar_wind(fname)
+            _df['time'] = ftimes[i_f]
+            dfs.append(_df)
+        df = pd.concat(dfs)
+    else:
+        df = _read_lidar_wind(fnames)
+        df['time'] = ftimes
+    
+    if dropna:
+        df = df.dropna()
+    
+    df = df.reset_index(drop=True)
+    
+    return df
+
+def _read_wpr_kma(fname):
+    from . import util
+    
+    with open(fname) as f:
+        _ = f.readline()
+        data = f.readline()
+    split = data.split('#')
+    
+    df = pd.DataFrame({
+        'Alt': np.float_(split[2:-1:7]),
+        'WD': np.float_(split[3::7]),
+        'WS': np.float_(split[4::7]),
+        'U': np.float_(split[5::7]),
+        'V': np.float_(split[6::7]),
+        'Uknot': util.ms2knot(np.float_(split[5::7])),
+        'Vknot': util.ms2knot(np.float_(split[6::7])),
+        'time': None
+    })
+    
+    return df
+
+def read_wpr_kma(fnames, ftimes, dropna=True):
+    """
+    Read KMA wind profiler.
+    
+    Examples
+    ---------
+    >>> df = kkpy.io.read_wpr_kma(fnames)
+    
+    Parameters
+    ----------
+    fnames : str or array_like
+        Filename(s) of KMA wind profiler data to read.
+    ftimes : str or array_like
+        Datetime(s) of KMA wind profiler data to read (see: kkpy.io.get_fname).
+    dropna : boolean (optional)
+        True if drop rows containing NaN values.
+    
+    Returns
+    ---------
+    df : pandas dataframe object
+        Return KMA wind profiler data dataframe.
+    
+    Notes
+    ---------
+    Columns:
+        - 'Alt': altitude from wind profiler [m]
+        - 'WD': wind direction [deg]
+        - 'WS': wind speed [m s-1]
+        - 'U': u-wind [m s-1]
+        - 'V': v-wind [m s-1]
+        - 'Uknot': u-wind [knot]
+        - 'Vknot': v-wind [knot]
+        - 'time': measurement time [file-dependent, generally KST]
+    """
+    dfs = []
+    
+    if isinstance(fnames, (list, np.ndarray)):
+        for i_f, fname in enumerate(fnames):
+            _df = _read_wpr_kma(fname)
+            _df['time'] = ftimes[i_f]
+            dfs.append(_df)
+        df = pd.concat(dfs)
+    else:
+        df = _read_wpr_kma(fnames)
+        df['time'] = ftimes
+    
+    if dropna:
+        df = df.dropna()
+    
+    df = df.reset_index(drop=True)
+    
+    return df
+
+def _read_pluvio_raw(fname):
+    df = pd.read_csv(
+        fname,
+        skiprows=26,
+        names=[
+            'Date', 'Time', 'STIN',
+            'PRT', 'PA', 'PNRT',
+            'PATNRT', 'BRT', 'BNRT',
+            'T', 'HStatus', 'Status',
+            'dummy1', 'dummy2', 'dummy3'],
+        delim_whitespace=True,
+    )
+    df['time'] = pd.to_datetime(
+        df['Date']+df['Time'],
+        format='%Y/%m/%d%H:%M:%S'
+    )
+    df = df.drop([
+        'STIN','HStatus','Status',
+        'Date','Time',
+        'dummy1','dummy2','dummy3'],
+        axis=1)
+    return df
+
+def read_pluvio_raw(fnames, dropna=True):
+    """
+    Read pluvio raw data.
+    
+    Examples
+    ---------
+    >>> df = kkpy.io.read_pluvio(fnames)
+    
+    Parameters
+    ----------
+    fnames : str or array_like
+        Filename(s) of pluvio raw data to read.
+    dropna : boolean (optional)
+        True if drop rows containing NaN values.
+    
+    Returns
+    ---------
+    df : pandas dataframe object
+        Return pluvio raw data dataframe.
+    
+    Notes
+    ---------
+    Columns:
+        - 'PRT': Intensity RT [mm hr-1]
+        - 'PA': Accu RT-NRT [mm]
+        - 'PNRT': Accu NRT [mm]
+        - 'PATNRT': Accu total NRT [mm]
+        - 'BRT': Bucket RT [mm]
+        - 'BNRT': Bucket NRT [mm]
+        - 'T': Temperature of load cell [degC]
+        - 'time': measurement time [file-dependent, generally UTC]
+    See details at OTT manual (pluvio operating instructions). The variable names are based on the document number 70.020.000.B.E 04-0515.
+    """
+    dfs = []
+    
+    if isinstance(fnames, (list, np.ndarray)):
+        for i_f, fname in enumerate(fnames):
+            _df = _read_pluvio_raw(fname)
+            dfs.append(_df)
+        df = pd.concat(dfs)
+    else:
+        df = _read_pluvio_raw(fnames)
+
+    if dropna:
+        df = df.dropna()
+    
+    df = df.reset_index(drop=True)
+    
+    return df
